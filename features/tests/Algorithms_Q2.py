aecfb0c7871a7e5e736ddb9b442866dc46127670
Maria Marques <sbn.marques55@gmail.com>
2021-06-19 12:45:43 -0400

    update tests/Algorithms_Q2.py

---
+++ features/tests/Algorithms_Q2.py
@@ -0,0 +1,12 @@
+# Find max number from 3 values, entered manually from a keyboard.
+
+a = int(input('Enter first integer'))
+b = int(input('Enter second integer'))
+c = int(input('Enter third integer'))
+print('Maximo is ', end='')
+if b <= a >= c:
+    print(a)
+elif a <= b >= c:
+    print(b)
+elif a <= b >= b:
+    print(c)
