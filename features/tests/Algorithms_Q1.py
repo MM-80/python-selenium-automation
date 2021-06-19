4f7a942cdc1d156d7cf68adf79b837ef77b5b19f
Maria Marques <sbn.marques55@gmail.com>
2021-06-19 12:37:31 -0400

    update tests/Algorithms_Q1.py

---
+++ features/tests/Algorithms_Q1.py
@@ -0,0 +1,11 @@
+# Compute the sum of digits in all numbers from 1 to n. When a user enters
+# a number n, find the sum of digits in all numbers from 1 to n.
+
+n = int(input('Enter a number:'))
+total = 0
+while n > 0:
+    digit = n % 10
+    total = total+digit
+    n = n//10
+print("The total sum of digits is:", total)
+
