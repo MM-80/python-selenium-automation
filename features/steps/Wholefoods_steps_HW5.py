c46dd1b5f53faf65c16170cf67bdb7e7e8acc9ee
Maria Marques <sbn.marques55@gmail.com>
2021-06-19 08:11:23 -0400

    updatewholefoods_cenario_WH5.feature

---
+++ features/steps/Wholefoods_steps_HW5.py
@@ -0,0 +1,18 @@
+
+
+from behave import given, then
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+@given('Open Amazon wholefoods deals page')
+def open_amazon_wholefoods(context):
+    context.driver = webdriver.Chrome(executable_path='/Users/mariamarques/python-selenium-automation/chromedriver')
+    context.driver.get('https://www.amazon.com/wholefoodsdeals')
+
+
+@then('Verify that every product has a text ‘Regular’ and a name')
+def open_amazon_product(context):
+    context.driver.implicitly_wait(15)
+    context.driver.find_elements(By.CSS_SELECTOR, "div.a-section.a-padding-none.a-text-center.wfm-desktop-section-size div[class='s-item-container']")
+    'Regular'
---
+++ features/wholefoods_cenario_WH5.feature
@@ -0,0 +1,6 @@
+# Created by mariamarques at 6/16/21
+Feature: # Enter feature name here
+  # Enter feature description here
+
+  Scenario: # Enter scenario name here
+    # Enter steps here
