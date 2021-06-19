c92ac9fab63c64451f2e47eafb5f4d153d6796eb
Maria Marques <sbn.marques55@gmail.com>
2021-06-19 13:44:50 -0400

    update wholefoods_cenario_WH5.feature

--- features/tests/cancel_order_script_hw3.py
+++ features/tests/cancel_order_script_hw3.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 # init driver
-driver = webdriver.Chrome(executable_path='/Users/mariamarques/python-selenium-automation/chromedriver')
+driver = webdriver.Chrome(executable_path='/chromedriver')
 driver.maximize_window()
 
 # open the url
